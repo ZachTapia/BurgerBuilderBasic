6fe94e60214db05de559112afb2860eed107a633
Zach Tapia <zachtapia@gmail.com>
2020-01-17 10:02:56 -0800

    Created Burger and BurgerIngredient components

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Auxiliary from '../../hoc/Aux';
+import classes from './Layout.module.css';
+
+const Layout = props => (
+  <Auxiliary>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Auxiliary>
+);
+
+export default Layout;
