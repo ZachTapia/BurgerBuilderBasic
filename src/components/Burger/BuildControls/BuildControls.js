fce8117e09f5df73b30de00955e3a7feb38b5aec
Zach Tapia <zachtapia@gmail.com>
2020-01-20 14:18:21 -0800

    Added 'Order Now' Button, implemented 'purchasable' state

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,31 +5,34 @@ import BuildControl from './BuildControl/BuildControl';
 
 // Just an object to iterate over, they could just be hardcoded instead
 const controls = [
-  { label: 'Salad', type: 'salad' },
-  { label: 'Bacon', type: 'bacon' },
-  { label: 'Cheese', type: 'cheese' },
-  { label: 'Meat', type: 'meat' }
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
 ];
 
 const BuildControls = props => {
-  return (
-    <div className={classes.BuildControls}>
-      <p>
-        Current Price: <strong>{props.price.toFixed(2)}</strong>
-      </p>
-      {controls.map(control => {
-        return (
-          <BuildControl
-            key={control.label}
-            label={control.label}
-            ingredientAdded={() => props.ingredientAdded(control.type)}
-            ingredientRemoved={() => props.ingredientRemoved(control.type)}
-            disabled={props.disabled[control.type]}
-          />
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className={classes.BuildControls}>
+            <p>
+                Current Price: <strong>{props.price.toFixed(2)}</strong>
+            </p>
+            {controls.map(control => {
+                return (
+                    <BuildControl
+                        key={control.label}
+                        label={control.label}
+                        ingredientAdded={() => props.ingredientAdded(control.type)}
+                        ingredientRemoved={() => props.ingredientRemoved(control.type)}
+                        disabled={props.disabled[control.type]}
+                    />
+                );
+            })}
+            <button className={classes.OrderButton} disabled={!props.purchasable}>
+                ORDER NOW
+            </button>
+        </div>
+    );
 };
 
 export default BuildControls;
