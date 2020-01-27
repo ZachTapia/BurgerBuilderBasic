753fd258fb1a3c44fd17845f42f73aa7a35d27a4
Zach Tapia <zachtapia@gmail.com>
2020-01-27 14:24:09 -0800

    Implemented side drawer for mobile, also implemented hamburger button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => (
-    <Auxiliary>
-        <Toolbar />
-        <main className={classes.Content}>{props.children}</main>
-    </Auxiliary>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
+
+    SideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    };
+
+    SideDrawerToggleHandler = () => {
+        this.setState(prevState => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    };
+
+    render() {
+        return (
+            <Auxiliary>
+                <Toolbar drawerToggleClicked={this.SideDrawerToggleHandler} />
+                <SideDrawer close={this.SideDrawerClosedHandler} open={this.state.showSideDrawer} />
+                <main className={classes.Content}>{this.props.children}</main>
+            </Auxiliary>
+        );
+    }
+}
 
 export default Layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Logo from '../../../components/Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
+import classes from './SideDrawer.module.css';
+
+const SideDrawer = props => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return (
+        <Auxiliary>
+            <Backdrop show={props.open} clicked={props.close} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Auxiliary>
+    );
+};
+
+export default SideDrawer;
